756b4f269fc93a7017e5417e12bcf26cb279db08
Daniel Medrano <daniel@quicktortoise.com>
2020-09-09 13:41:17 -0500

    Agregar propiedades
    bool AreIdentical para indicar si los arreglos comparados son iguales.
    byte[] IdenticalFirstBytes y byte[] IdenticalFinalBytes para almacenar el resultado de la comparación en caso de que no sean iguales.

--- CompareBytes/ByteComparison.cs
+++ CompareBytes/ByteComparison.cs
@@ -2,5 +2,8 @@ namespace CompareBytes
 {
     public class ByteComparison
     {
+        public bool AreIdentical { get; set; }
+        public byte[] IdenticalFirstBytes { get; set; }
+        public byte[] IdenticalFinalBytes { get; set; }
     }
 }
