1b0abf4a626bdc055d70d8cb3900af6dad0deec1
Daniel Medrano <daniel@quicktortoise.com>
2020-09-09 13:52:40 -0500

    Agregar método estático ByteComparison CompareBytes(byte[], byte[])
    Agregar constructor privado, hacer privada la escritura de las propiedades.

--- CompareBytes/ByteComparison.cs
+++ CompareBytes/ByteComparison.cs
@@ -2,9 +2,39 @@ namespace CompareBytes
 {
     public class ByteComparison
     {
-        public bool AreIdentical { get; set; }
-        public byte[] IdenticalFirstBytes { get; set; }
-        public byte[] IdenticalFinalBytes { get; set; }
+        private ByteComparison()
+        {
+        }
+
+        public bool AreIdentical { get; private set; }
+        public byte[] IdenticalFirstBytes { get; private set; }
+        public byte[] IdenticalFinalBytes { get; private set; }
+
+        public static ByteComparison CompareBytes(byte[] byteArrayA, byte[] byteArrayB)
+        {
+            ByteComparison comparison;
+
+            if (ByteArrayUtils.Equal(byteArrayA, byteArrayB))
+            {
+                comparison = new ByteComparison
+                {
+                    AreIdentical = true,
+                    IdenticalFirstBytes = null,
+                    IdenticalFinalBytes = null
+                };
+            }
+            else
+            {
+                comparison = new ByteComparison
+                {
+                    AreIdentical = false,
+                    IdenticalFirstBytes = CompareFirstBytes(byteArrayA, byteArrayB),
+                    IdenticalFinalBytes = CompareLastBytes(byteArrayA, byteArrayB)
+                };
+            }
+
+            return comparison;
+        }
 
         public static byte[] CompareFirstBytes(byte[] byteArrayA, byte[] byteArrayB)
         {
@@ -31,7 +61,7 @@ namespace CompareBytes
 
             return resultArray;
         }
-        
+
         public static byte[] CompareLastBytes(byte[] byteArrayA, byte[] byteArrayB)
         {
             if (byteArrayA == null || byteArrayB == null) return new byte[0];
