b0b381fbdf365207e617f219ab9426bbd8e35623
Daniel Medrano <daniel@quicktortoise.com>
2020-09-09 13:46:18 -0500

    Agregar método estático byte[] CompareLastBytes(byte[], byte[])

--- CompareBytes/ByteComparison.cs
+++ CompareBytes/ByteComparison.cs
@@ -31,5 +31,31 @@ namespace CompareBytes
 
             return resultArray;
         }
+        
+        public static byte[] CompareLastBytes(byte[] byteArrayA, byte[] byteArrayB)
+        {
+            if (byteArrayA == null || byteArrayB == null) return new byte[0];
+
+            byte[] resultArray;
+            int count;
+            if (byteArrayA.Length > byteArrayB.Length)
+            {
+                for (count = 0; count < byteArrayB.Length; count++)
+                    if (byteArrayA[byteArrayA.Length - count - 1] != byteArrayB[byteArrayB.Length - count - 1])
+                        break;
+            }
+            else
+            {
+                for (count = 0; count < byteArrayA.Length; count++)
+                    if (byteArrayA[byteArrayA.Length - count - 1] != byteArrayB[byteArrayB.Length - count - 1])
+                        break;
+            }
+
+            resultArray = new byte[count];
+            for (int j = 0; j < count; j++)
+                resultArray[count - j - 1] = byteArrayA[byteArrayA.Length - j - 1];
+
+            return resultArray;
+        }
     }
 }
