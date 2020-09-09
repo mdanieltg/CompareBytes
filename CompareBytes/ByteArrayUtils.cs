3b0ddfa1f185150127c164e586bb59761e925321
Daniel Medrano <daniel@quicktortoise.com>
2020-09-09 13:34:22 -0500

    Agregar método estático bool Equal(byte[], byte[])

--- CompareBytes/ByteArrayUtils.cs
+++ CompareBytes/ByteArrayUtils.cs
@@ -2,5 +2,17 @@ namespace CompareBytes
 {
     public static class ByteArrayUtils
     {
+        public static bool Equal(byte[] byteArrayA, byte[] byteArrayB)
+        {
+            if (byteArrayA == null || byteArrayB == null) return false;
+            if (byteArrayA.Length != byteArrayB.Length) return false;
+            int arraySize = byteArrayA.Length;
+
+            for (int i = 0; i < arraySize; i++)
+                if (byteArrayA[i] != byteArrayB[i])
+                    return false;
+
+            return true;
+        }
     }
 }
