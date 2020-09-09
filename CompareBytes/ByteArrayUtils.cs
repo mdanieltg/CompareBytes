12c1aa84f50219d7e936a84ce6fda849cd988932
Daniel Medrano <daniel@quicktortoise.com>
2020-09-09 13:37:07 -0500

    Agregar método string ArrayToString(byte[], PrintFormat)
    Agregar también enumeración PrintFormat{ Decimal, Hexadecimal}

--- CompareBytes/ByteArrayUtils.cs
+++ CompareBytes/ByteArrayUtils.cs
@@ -2,6 +2,30 @@ namespace CompareBytes
 {
     public static class ByteArrayUtils
     {
+        public enum PrintFormat
+        {
+            Decimal,
+            Hexadecimal
+        }
+
+        public static string ArrayToString(byte[] byteArray, PrintFormat printFormat = PrintFormat.Decimal)
+        {
+            var stringBuilder = new System.Text.StringBuilder();
+
+            for (int i = 0; i < byteArray.Length; i++)
+            {
+                if (printFormat == PrintFormat.Hexadecimal)
+                    stringBuilder.Append(byteArray[i].ToString("x2"));
+                else
+                    stringBuilder.Append(byteArray[i].ToString("D"));
+
+                // Add space separator
+                if (i + 1 < byteArray.Length) stringBuilder.Append(' ');
+            }
+
+            return stringBuilder.ToString();
+        }
+
         public static bool Equal(byte[] byteArrayA, byte[] byteArrayB)
         {
             if (byteArrayA == null || byteArrayB == null) return false;
